647b7eaa69046abcc72529d1a0fa30d39844d1f4
umer <choudhry00641@gmail.com>
2025-08-29 16:06:48 +0500

    done new update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    domains: ['pakhims.com'], // ✅ Add your domain here
+  },
 };
 
 export default nextConfig;
