ba69007ad73642be5a43ffbdd4ab915db8d18561
wahab jutt <your-email@example.com>
2025-11-06 16:50:37 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   images: {
+  images: {
     domains: ['pakhims.com'], // ✅ Add your domain here
+    // Disable Next.js Image Optimization when exporting a static site
+    // See: https://nextjs.org/docs/messages/export-image-api
+    unoptimized: true,
   },
   output: 'export',
 };
